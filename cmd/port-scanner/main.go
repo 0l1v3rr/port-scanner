14cb218e2e3ed71dddc7279be2e4d4572374e66e
Olivér <oliver.mrakovics@gmail.com>
2021-09-16 16:51:51 +0200

    Added --protocol flag.

--- cmd/port-scanner/main.go
+++ cmd/port-scanner/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	pt "github.com/0l1v3rr/port-scanner/pkg/port"
 )
 
-func main() {
-	port := 80
-	open := pt.ScanPort("tcp", "localhost", port)
+const (
+	defaultProtocol string = "tcp"
+)
 
+var (
+	protocol string
+)
+
+func main() {
+	flag.StringVar(&protocol, "protocol", defaultProtocol, "The protocol.")
+	flag.Parse()
+
+	if protocol != "udp" && protocol != "tcp" {
+		fmt.Println("Error: Invalid protocol.")
+		return
+	}
+
+	port := 80
+	open := pt.ScanPort(protocol, "localhost", port)
+
+	fmt.Println("PORT \tSTATE \tSERVICE")
 	if open {
-		fmt.Printf("The port %v is opened.", port)
+		fmt.Printf("%v/%v \topen \thttp", port, protocol)
 	} else {
-		fmt.Printf("The port %v is closed.", port)
+		fmt.Printf("%v/%v \tclosed \thttp", port, protocol)
 	}
 
 }
