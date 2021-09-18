65c14a3472663deeaf6969b4f51f40da00b09cf5
Olivér <oliver.mrakovics@gmail.com>
2021-09-18 09:09:18 +0200

    Updated portservice.go

--- pkg/port/portservice.go
+++ pkg/port/portservice.go
@@ -100,6 +100,106 @@ func PortServiceName(port int) string {
 		return "ripng (ipv6)"
 	case 540:
 		return "uucp"
+	case 554:
+		return "rtsp"
+	case 546:
+		return "dhcpv6"
+	case 547:
+		return "dhcpv6"
+	case 560:
+		return "rmonitor"
+	case 563:
+		return "nntp over ssl"
+	case 587:
+		return "smtp"
+	case 591:
+		return "filemaker"
+	case 593:
+		return "microsoft dcom"
+	case 631:
+		return "internet printing"
+	case 636:
+		return "dlap over ssl"
+	case 639:
+		return "msdp (pim)"
+	case 646:
+		return "ldp (mpls)"
+	case 691:
+		return "ms exchange"
+	case 860:
+		return "iscsi"
+	case 873:
+		return "rsync"
+	case 902:
+		return "vmware server"
+	case 989:
+		return "ftp over ssl"
+	case 990:
+		return "ftp over ssl"
+	case 993:
+		return "imap4 over ssl"
+	case 995:
+		return "pop3 over ssl"
+	case 1025:
+		return "microsoft rpc"
+	case 1080:
+		return "socks proxy/mydooom"
+	case 1194:
+		return "openvpn"
+	case 1214:
+		return "kazaa"
+	case 1241:
+		return "nessus"
+	case 1311:
+		return "deil openmanage"
+	case 1337:
+		return "waste"
+	case 1433:
+		return "microsoft sql"
+	case 1434:
+		return "microsoft sql"
+	case 1512:
+		return "wins"
+	case 1589:
+		return "cisco vqp"
+	case 1701:
+		return "l2tp"
+	case 1723:
+		return "ms pptp"
+	case 1725:
+		return "steam"
+	case 1741:
+		return "ciscoworks 2000"
+	case 1755:
+		return "ms media server"
+	case 1812:
+		return "radius"
+	case 1813:
+		return "radius"
+	case 1863:
+		return "msn"
+	case 1985:
+		return "cisco hsrp"
+	case 2000:
+		return "cisco scco"
+	case 2002:
+		return "cisco asc"
+	case 2049:
+		return "nfs"
+	case 2082:
+		return "cpanel"
+	case 2083:
+		return "cpanel"
+	case 2100:
+		return "oracle xdb"
+	case 2222:
+		return "directadmin"
+	case 2302:
+		return "halo"
+	case 2483:
+		return "oracle db"
+	case 2484:
+		return "oracle db"
 	}
 
 	return "unknown"
