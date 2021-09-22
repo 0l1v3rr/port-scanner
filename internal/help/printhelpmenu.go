d6027801fdfd86313b2331ef806d64b1a58e57b4
Olivér <oliver.mrakovics@gmail.com>
2021-09-22 17:22:03 +0200

    Updated the help menu.

--- internal/help/printhelpmenu.go
+++ internal/help/printhelpmenu.go
@@ -3,11 +3,24 @@ package help
 import "fmt"
 
 func PrintHelpMenu() {
-	fmt.Println(" ───────────────────────────────────")
-	fmt.Println("\tPORT SCANNER HELP MENU")
-	fmt.Println(" ───────────────────────────────────")
+	PrintLogo()
 	fmt.Println(" --protocol \t The protocol (tcp/udp). Default is TCP.")
 	fmt.Println(" --ip \t\t The IP Address you want to scan. Default is 'localhost'.")
 	fmt.Println(" --port \t The only port you want to scan.")
+	fmt.Println(" --closed \t With this flag, the app won't show the closed ports.")
 	fmt.Println("")
+}
+
+func PrintLogo() {
+	fmt.Println()
+	fmt.Println("██▓███   ▒█████   ██▀███  ▄▄▄█████▓     ██████  ▄████▄   ▄▄▄       ███▄    █  ███▄    █ ▓█████  ██▀███  ")
+	fmt.Println("▓██░  ██▒▒██▒  ██▒▓██ ▒ ██▒▓  ██▒ ▓▒   ▒██    ▒ ▒██▀ ▀█  ▒████▄     ██ ▀█   █  ██ ▀█   █ ▓█   ▀ ▓██ ▒ ██▒")
+	fmt.Println("▓██░ ██▓▒▒██░  ██▒▓██ ░▄█ ▒▒ ▓██░ ▒░   ░ ▓██▄   ▒▓█    ▄ ▒██  ▀█▄  ▓██  ▀█ ██▒▓██  ▀█ ██▒▒███   ▓██ ░▄█ ▒")
+	fmt.Println("▒██▄█▓▒ ▒▒██   ██░▒██▀▀█▄  ░ ▓██▓ ░      ▒   ██▒▒▓▓▄ ▄██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒▓██▒  ▐▌██▒▒▓█  ▄ ▒██▀▀█▄  ")
+	fmt.Println("▒██▒ ░  ░░ ████▓▒░░██▓ ▒██▒  ▒██▒ ░    ▒██████▒▒▒ ▓███▀ ░ ▓█   ▓██▒▒██░   ▓██░▒██░   ▓██░░▒████▒░██▓ ▒██▒")
+	fmt.Println("▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░  ▒ ░░      ▒ ▒▓▒ ▒ ░░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░")
+	fmt.Println("░▒ ░       ░ ▒ ▒░   ░▒ ░ ▒░    ░       ░ ░▒  ░ ░  ░  ▒     ▒   ▒▒ ░░ ░░   ░ ▒░░ ░░   ░ ▒░ ░ ░  ░  ░▒ ░ ▒░")
+	fmt.Println("░░       ░ ░ ░ ▒    ░░   ░   ░         ░  ░  ░  ░          ░   ▒      ░   ░ ░    ░   ░ ░    ░     ░░   ░ ")
+	fmt.Println("             ░ ░     ░                       ░  ░ ░            ░  ░         ░          ░    ░  ░   ░     ")
+	fmt.Println("                                                ░                                                        ")
 }
